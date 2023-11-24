7f2e560ad4dea623133e3999ca0f530f4c08bc7d
Esusbhatu <Haben1414@gmail.com>
2023-11-25 00:40:37 +0100

    langular adjustment

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
-log = "background.jpg";
+log = "sofanishop.jpeg";
 
 constructor( private route: ActivatedRoute, 
   private router: Router){
